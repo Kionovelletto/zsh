4f762fd64abdb24f38637f204bcb735d25bf0ec3
Caio H. Novelletto <kionovelletto@gmail.com>
2023-08-14 18:56:11 -0300

    Update install_zsh_bira.sh

--- install_zsh_bira.sh
+++ install_zsh_bira.sh
@@ -6,27 +6,34 @@ OLD_THEME="robbyrussell"
 NEW_THEME="bira"
 NEW_PLUGINS="git zsh-autosuggestions zsh-syntax-highlighting"
 
-sudo apt install zsh -y
-chsh -s /bin/zsh
-zsh
+echo -e "..10%"
+sudo apt install zsh -y >/dev/null
+chsh -s /bin/zsh >/dev/null
+zsh >/dev/null
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo -e ".....25%"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >/dev/null
 
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+echo -e ".......40%"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 >/dev/null
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme" >/dev/null
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo -e "..........60%"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions >/dev/null
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo -e "............70%"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting >/dev/null
 
+echo -e "..............80%"
 if [ -f "$ZSHRC_FILE" ]; then
     sed -i "s/ZSH_THEME=\"$OLD_THEME\"/ZSH_THEME=\"$NEW_THEME\"/" "$ZSHRC_FILE"
-    echo "Tema do Zsh substituido de \"$OLD_THEME\" para \"$NEW_THEME\"."
-    echo "Lembre-se de recarregar o Zsh para que as alterações tenham efeito."
+    echo "Tema do Zsh substituido de \"$OLD_THEME\" para \"$NEW_THEME\"." 
+    echo "Lembre-se de recarregar o Zsh para que as alterações tenham efeito." 
 else
     echo "Arquivo $ZSHRC_FILE não localizado."
 fi
 
+echo -e "................90%"
 if [ -f "$ZSHRC_FILE" ]; then
     sed -i "s/plugins=(git)/plugins=($NEW_PLUGINS)/" "$ZSHRC_FILE"
     echo "Plugins do Zsh substituido: $NEW_PLUGINS."
@@ -34,3 +41,5 @@ if [ -f "$ZSHRC_FILE" ]; then
 else
     echo "Arquivo $ZSHRC_FILE não encontrado."
 fi
+echo -e "..................100%"
+echo -e "Done !"
